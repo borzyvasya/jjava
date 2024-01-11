d21483fb933488caf7f81d4c9e381f0acad3dc02
scarabush_scarabush <snusikfrescus@gmail.com>
2024-01-11 06:51:00 +0300

    few2

--- bess/src/Prikscok.java
+++ bess/src/Prikscok.java
@@ -2,6 +2,9 @@ public class Prikscok {
     public static void main(String[] args) {
         int a = 5;
 
-        System.out.println(1);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
