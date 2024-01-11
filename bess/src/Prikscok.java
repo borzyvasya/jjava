5eb18198b492a088d23b98852366db85ccac438e
scarabush_scarabush <snusikfrescus@gmail.com>
2024-01-11 07:22:37 +0300

    few4

--- bess/src/Prikscok.java
+++ bess/src/Prikscok.java
@@ -7,5 +7,16 @@ public class Prikscok {
         System.out.println(1);
         System.out.println(1);
         System.out.println(1);
+
+
+
+
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+
+
     }
 }
