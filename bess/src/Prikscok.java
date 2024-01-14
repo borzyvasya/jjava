0b2947f4a11b52aea35106f5c83389cb4d397303
scarabush_scarabush <snusikfrescus@gmail.com>
2024-01-14 23:23:47 +0300

    few5

--- bess/src/Prikscok.java
+++ bess/src/Prikscok.java
@@ -1,8 +1,8 @@
+import javax.swing.*;
+
 public class Prikscok {
     public static void main(String[] args) {
-        int a = 5;
-
-        System.out.println(1);
+        JFrame frame = new JFrame();
 
     }
 }
